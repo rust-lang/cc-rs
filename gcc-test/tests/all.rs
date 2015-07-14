2c810db5945527fe13a2090bbc60f6fe11c63c43
Alex Crichton <alex@alexcrichton.com>
2015-07-14 15:35:07 -0700

    Fix CI

---
+++ gcc-test/tests/all.rs
@@ -0,0 +1,19 @@
+extern crate gcc_test;
+
+use gcc_test::*;
+
+#[test]
+fn foo_here() {
+    unsafe {
+        assert_eq!(foo(), 4);
+    }
+}
+
+#[test]
+fn bar_here() {
+    unsafe {
+        assert_eq!(bar1(), 5);
+        assert_eq!(bar2(), 6);
+    }
+}
+
