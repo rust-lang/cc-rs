047bf5b1285251f392c5e1a34dfcc4070a7158a8
Stephan Seitz <stephan.seitz@fau.de>
2019-10-11 21:53:15 +0200

    Fix typo in lib.rs doc

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@
 //! Cargo will also set this environment variable when executed with the `-jN` flag.
 //!
 //! If `NUM_JOBS` is not set, the `RAYON_NUM_THREADS` environment variable can
-//! also specify the build paralellism.
+//! also specify the build parallelism.
 //!
 //! # Examples
 //!
