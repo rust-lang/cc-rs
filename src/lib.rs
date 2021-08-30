571218e90e39009295aa4610d5343c21a9fd83f3
Eran Rundstein <eran@rundste.in>
2021-08-30 12:22:45 -0700

    always use ios13.0 target for catalyst builds

--- src/lib.rs
+++ src/lib.rs
@@ -1535,19 +1535,8 @@ impl Build {
                         if let Some(arch) =
                             map_darwin_target_from_rust_to_compiler_architecture(target)
                         {
-                            let deployment_target = env::var("CATALYST_DEPLOYMENT_TARGET")
-                                .unwrap_or_else(|_| {
-                                    if arch == "arm64" {
-                                        "".into()
-                                    } else {
-                                        "13.0".into()
-                                    }
-                                });
-
-                            cmd.args.push(
-                                format!("--target={}-apple-ios{}-macabi", arch, deployment_target)
-                                    .into(),
-                            );
+                            cmd.args
+                                .push(format!("--target={}-apple-ios13.0-macabi", arch).into());
                         }
                     } else if target.contains("ios-sim") {
                         if let Some(arch) =
