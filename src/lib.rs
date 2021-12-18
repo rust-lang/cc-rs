cfa2606f9227d0d602b06b80d909bb219f1d240d
Sébastien Marie <semarie@online.fr>
2021-12-18 12:24:49 +0000

    Support riscv64-unknown-openbsd

--- src/lib.rs
+++ src/lib.rs
@@ -1790,6 +1790,9 @@ impl Build {
                         } else if target.contains("freebsd") && arch.starts_with("64") {
                             cmd.args.push(("-march=rv64gc").into());
                             cmd.args.push("-mabi=lp64d".into());
+                        } else if target.contains("openbsd") && arch.starts_with("64") {
+                            cmd.args.push(("-march=rv64gc").into());
+                            cmd.args.push("-mabi=lp64d".into());
                         } else if target.contains("linux") && arch.starts_with("32") {
                             cmd.args.push(("-march=rv32gc").into());
                             cmd.args.push("-mabi=ilp32d".into());
