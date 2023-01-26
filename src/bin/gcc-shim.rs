7825c87557592801592c999b27c3d6200e5947d7
Jiahao XU <Jiahao_XU@outlook.com>
2023-01-26 18:29:53 +1100

    Rm `EnumUnwrapExt` in `bin/gcc-shim.rs`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/bin/gcc-shim.rs
+++ src/bin/gcc-shim.rs
@@ -1,67 +1,9 @@
 #![cfg_attr(test, allow(dead_code))]
 
 use std::env;
-use std::fmt;
 use std::fs::File;
 use std::io::{self, prelude::*};
 use std::path::PathBuf;
-
-trait EnumUnwrapExt {
-    type Value;
-
-    fn expect_fmt(self, args: fmt::Arguments<'_>) -> Self::Value;
-}
-
-/// This is a separate function to reduce the code size of the methods
-///
-/// Adapted from libstd
-/// https://doc.rust-lang.org/src/core/result.rs.html#1785-1792
-#[inline(never)]
-#[cold]
-fn unwrap_failed(msg: fmt::Arguments<'_>, error: &dyn fmt::Debug) -> ! {
-    panic!("{}: {:?}", msg, error)
-}
-
-impl<T, E> EnumUnwrapExt for Result<T, E>
-where
-    E: fmt::Debug,
-{
-    type Value = T;
-
-    /// Adapted from libstd
-    /// https://doc.rust-lang.org/src/core/result.rs.html#1064-1066
-    #[inline]
-    fn expect_fmt(self, msg: fmt::Arguments<'_>) -> T {
-        match self {
-            Ok(t) => t,
-            Err(e) => unwrap_failed(msg, &e),
-        }
-    }
-}
-
-/// This is a separate function to reduce the code size of .expect() itself.
-///
-/// Adapted from libstd
-/// https://doc.rust-lang.org/src/core/option.rs.html#1872-1880
-#[inline(never)]
-#[cold]
-fn expect_failed(msg: fmt::Arguments<'_>) -> ! {
-    panic!("{}", msg)
-}
-
-impl<T> EnumUnwrapExt for Option<T> {
-    type Value = T;
-
-    /// Adapted from libstd
-    /// https://doc.rust-lang.org/src/core/option.rs.html#734-743
-    #[inline]
-    fn expect_fmt(self, msg: fmt::Arguments<'_>) -> T {
-        match self {
-            Some(val) => val,
-            None => expect_failed(msg),
-        }
-    }
-}
 
 fn main() {
     let mut args = env::args();
@@ -69,7 +11,7 @@ fn main() {
 
     let out_dir = PathBuf::from(
         env::var_os("GCCTEST_OUT_DIR")
-            .expect_fmt(format_args!("{}: GCCTEST_OUT_DIR not found", program)),
+            .unwrap_or_else(|| panic!("{}: GCCTEST_OUT_DIR not found", program)),
     );
 
     // Find the first nonexistent candidate file to which the program's args can be written.
@@ -82,14 +24,17 @@ fn main() {
         } else {
             Some(candidate)
         }
-    }).expect_fmt(format_args!("Cannot find the first nonexistent candidate file to which the program's args can be written under out_dir '{}'", out_dir.display()));
+    }).unwrap_or_else(|| panic!("Cannot find the first nonexistent candidate file to which the program's args can be written under out_dir '{}'", out_dir.display()));
 
     // Create a file and record the args passed to the command.
-    let f = File::create(&candidate).expect_fmt(format_args!(
-        "{}: can't create candidate: {}",
-        program,
-        candidate.display()
-    ));
+    let f = File::create(&candidate).unwrap_or_else(|e| {
+        panic!(
+            "{}: can't create candidate: {}, error: {}",
+            program,
+            candidate.display(),
+            e
+        )
+    });
     let mut f = io::BufWriter::new(f);
 
     (|| {
@@ -103,17 +48,23 @@ fn main() {
         f.flush()?;
         f.sync_all()
     })()
-    .expect_fmt(format_args!(
-        "{}: can't write to candidate: {}",
-        program,
-        candidate.display()
-    ));
+    .unwrap_or_else(|e| {
+        panic!(
+            "{}: can't write to candidate: {}, error: {}",
+            program,
+            candidate.display(),
+            e
+        )
+    });
 
     // Create a file used by some tests.
     let path = &out_dir.join("libfoo.a");
-    File::create(path).expect_fmt(format_args!(
-        "{}: can't create libfoo.a: {}",
-        program,
-        path.display()
-    ));
+    File::create(path).unwrap_or_else(|e| {
+        panic!(
+            "{}: can't create libfoo.a: {}, error: {}",
+            program,
+            path.display(),
+            e
+        )
+    });
 }
