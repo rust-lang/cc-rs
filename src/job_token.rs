c3e577ff3ecbbf4423c78bf26f5eb4d9759366e8
Jiahao XU <Jiahao_XU@outlook.com>
2023-10-21 23:13:01 +1100

    Impl vendored jobserver implementation
    It supports non-blocking `try_acquire` and is much simpler than the one
    provided by `jobserver`
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/job_token.rs
+++ src/job_token.rs
@@ -1,29 +1,16 @@
-use jobserver::{Acquired, Client, HelperThread};
-use std::{
-    env,
-    mem::MaybeUninit,
-    sync::{
-        mpsc::{self, Receiver, Sender},
-        Once,
-    },
-};
+use std::{mem::MaybeUninit, sync::Once};
 
-pub(crate) struct JobToken {
-    /// The token can either be a fresh token obtained from the jobserver or - if `token` is None - an implicit token for this process.
-    /// Both are valid values to put into queue.
-    token: Option<Acquired>,
-    /// A pool to which `token` should be returned. `pool` is optional, as one might want to release a token straight away instead
-    /// of storing it back in the pool - see [`Self::forget()`] function for that.
-    pool: Option<Sender<Option<Result<Acquired, crate::Error>>>>,
-}
+#[cfg(unix)]
+#[path = "job_token/unix.rs"]
+mod sys;
 
-impl Drop for JobToken {
-    fn drop(&mut self) {
-        if let Some(pool) = &self.pool {
-            // Always send back an Ok() variant as we know that the acquisition for this token has succeeded.
-            let _ = pool.send(self.token.take().map(|token| Ok(token)));
-        }
-    }
+#[cfg(windows)]
+#[path = "job_token/windows.rs"]
+mod sys;
+
+pub(super) enum JobToken {
+    Inherited(inherited_jobserver::JobToken),
+    InProcess(inprocess_jobserver::JobToken),
 }
 
 impl JobToken {
@@ -31,109 +18,175 @@ impl JobToken {
     /// This also leads to releasing it sooner for other processes to use,
     /// which is a correct thing to do once it is known that there won't be
     /// any more token acquisitions.
-    pub(crate) fn forget(&mut self) {
-        self.pool.take();
+    pub(super) fn forget(&mut self) {
+        if let Self::Inherited(inherited_jobtoken) = self {
+            inherited_jobtoken.forget();
+        }
     }
 }
 
-/// A thin wrapper around jobserver's Client.
-/// It would be perfectly fine to just use jobserver's Client, but we also want to reuse
-/// our own implicit token assigned for this build script. This struct manages that and
-/// gives out tokens without exposing whether they're implicit tokens or tokens from jobserver.
-/// Furthermore, instead of giving up job tokens, it keeps them around
-/// for reuse if we know we're going to request another token after freeing the current one.
-pub(crate) struct JobTokenServer {
-    helper: HelperThread,
-    tx: Sender<Option<Result<Acquired, crate::Error>>>,
-    rx: Receiver<Option<Result<Acquired, crate::Error>>>,
+pub(super) enum JobTokenServer {
+    Inherited(inherited_jobserver::JobServer),
+    InProcess(inprocess_jobserver::JobServer),
 }
 
 impl JobTokenServer {
     pub(crate) fn new() -> &'static Self {
-        jobserver()
-    }
-    fn new_inner(client: Client) -> Result<Self, crate::Error> {
-        let (tx, rx) = mpsc::channel();
-        // Push the implicit token. Since JobTokens only give back what they got,
-        // there should be at most one global implicit token in the wild.
-        tx.send(None).unwrap();
-        let pool = tx.clone();
-        let helper = client.into_helper_thread(move |acq| {
-            let _ = pool.send(Some(acq.map_err(|e| e.into())));
-        })?;
-        Ok(Self { helper, tx, rx })
-    }
+        static INIT: Once = Once::new();
+        static mut JOBSERVER: MaybeUninit<JobTokenServer> = MaybeUninit::uninit();
 
-    pub(crate) fn acquire(&self) -> Result<JobToken, crate::Error> {
-        let token = if let Ok(token) = self.rx.try_recv() {
-            // Opportunistically check if there's a token that can be reused.
-            token
-        } else {
-            // Cold path, request a token and block
-            self.helper.request_token();
-            self.rx.recv().unwrap()
-        };
-        let token = if let Some(token) = token {
-            Some(token?)
-        } else {
-            None
-        };
-        Ok(JobToken {
-            token,
-            pool: Some(self.tx.clone()),
-        })
-    }
-}
-
-/// Returns a suitable `JobTokenServer` used to coordinate
-/// parallelism between build scripts. A global `JobTokenServer` is used as this ensures
-/// that only one implicit job token is used in the wild.
-/// Having multiple separate job token servers would lead to each of them assuming that they have control
-/// over the implicit job token.
-/// As it stands, each caller of `jobserver` can receive an implicit job token and there will be at most
-/// one implicit job token in the wild.
-fn jobserver() -> &'static JobTokenServer {
-    static INIT: Once = Once::new();
-    static mut JOBSERVER: MaybeUninit<JobTokenServer> = MaybeUninit::uninit();
-
-    fn _assert_sync<T: Sync>() {}
-    _assert_sync::<jobserver::Client>();
-
-    unsafe {
-        INIT.call_once(|| {
-            let server = default_jobserver();
-            JOBSERVER = MaybeUninit::new(
-                JobTokenServer::new_inner(server).expect("Job server initialization failed"),
-            );
-        });
-        // Poor man's assume_init_ref, as that'd require a MSRV of 1.55.
-        &*JOBSERVER.as_ptr()
-    }
-}
-
-unsafe fn default_jobserver() -> jobserver::Client {
-    // Try to use the environmental jobserver which Cargo typically
-    // initializes for us...
-    if let Some(client) = jobserver::Client::from_env() {
-        return client;
-    }
-
-    // ... but if that fails for whatever reason select something
-    // reasonable and crate a new jobserver. Use `NUM_JOBS` if set (it's
-    // configured by Cargo) and otherwise just fall back to a
-    // semi-reasonable number. Note that we could use `num_cpus` here
-    // but it's an extra dependency that will almost never be used, so
-    // it's generally not too worth it.
-    let mut parallelism = 4;
-    if let Ok(amt) = env::var("NUM_JOBS") {
-        if let Ok(amt) = amt.parse() {
-            parallelism = amt;
+        unsafe {
+            INIT.call_once(|| {
+                let server = inherited_jobserver::JobServer::from_env()
+                    .map(Self::Inherited)
+                    .unwrap_or_else(|| Self::InProcess(inprocess_jobserver::JobServer::new()));
+                JOBSERVER = MaybeUninit::new(server);
+            });
+            // TODO: Poor man's assume_init_ref, as that'd require a MSRV of 1.55.
+            &*JOBSERVER.as_ptr()
         }
     }
 
-    // If we create our own jobserver then be sure to reserve one token
-    // for ourselves.
-    let client = jobserver::Client::new(parallelism).expect("failed to create jobserver");
-    client.acquire_raw().expect("failed to acquire initial");
-    return client;
+    pub(crate) fn try_acquire(&self) -> Result<Option<JobToken>, crate::Error> {
+        match self {
+            Self::Inherited(jobserver) => jobserver
+                .try_acquire()
+                .map(|option| option.map(JobToken::Inherited)),
+            Self::InProcess(jobserver) => Ok(jobserver.try_acquire().map(JobToken::InProcess)),
+        }
+    }
+}
+
+mod inherited_jobserver {
+    use super::sys;
+
+    use std::{
+        env::var_os,
+        sync::mpsc::{self, Receiver, Sender},
+    };
+
+    pub(super) struct JobServer {
+        inner: sys::JobServerClient,
+        tx: Sender<Result<(), crate::Error>>,
+        rx: Receiver<Result<(), crate::Error>>,
+    }
+
+    impl JobServer {
+        pub(super) unsafe fn from_env() -> Option<Self> {
+            let var = var_os("CARGO_MAKEFLAGS")
+                .or_else(|| var_os("MAKEFLAGS"))
+                .or_else(|| var_os("MFLAGS"))?;
+
+            let inner = sys::JobServerClient::open(var)?;
+
+            let (tx, rx) = mpsc::channel();
+            // Push the implicit token. Since JobTokens only give back what they got,
+            // there should be at most one global implicit token in the wild.
+            tx.send(Ok(())).unwrap();
+
+            Some(Self { inner, tx, rx })
+        }
+
+        pub(super) fn try_acquire(&self) -> Result<Option<JobToken>, crate::Error> {
+            if let Ok(token) = self.rx.try_recv() {
+                // Opportunistically check if there's a token that can be reused.
+                token?
+            } else {
+                // Cold path, request a token
+                if self.inner.try_acquire()?.is_none() {
+                    return Ok(None);
+                }
+            };
+            Ok(Some(JobToken {
+                pool: Some(self.tx.clone()),
+                jobserver: self,
+            }))
+        }
+    }
+
+    /// A thin wrapper around jobserver Client.
+    /// It would be perfectly fine to just use jobserver Client, but we also want to reuse
+    /// our own implicit token assigned for this build script. This struct manages that and
+    /// gives out tokens without exposing whether they're implicit tokens or tokens from jobserver.
+    /// Furthermore, instead of giving up job tokens, it keeps them around
+    /// for reuse if we know we're going to request another token after freeing the current one.
+    pub(super) struct JobToken {
+        /// A pool to which `token` should be returned. `pool` is optional, as one might want to release a token straight away instead
+        /// of storing it back in the pool - see [`Self::forget()`] function for that.
+        pool: Option<Sender<Result<(), crate::Error>>>,
+        jobserver: &'static JobServer,
+    }
+
+    impl Drop for JobToken {
+        fn drop(&mut self) {
+            if let Some(pool) = &self.pool {
+                // Always send back an Ok() variant as we know that the acquisition for this token has succeeded.
+                let _ = pool.send(Ok(()));
+            } else {
+                let _ = self.jobserver.inner.release();
+            }
+        }
+    }
+
+    impl JobToken {
+        /// Ensure that this token is not put back into queue once it's dropped.
+        /// This also leads to releasing it sooner for other processes to use,
+        /// which is a correct thing to do once it is known that there won't be
+        /// any more token acquisitions.
+        pub(super) fn forget(&mut self) {
+            self.pool.take();
+        }
+    }
+}
+
+mod inprocess_jobserver {
+    use std::{
+        env::var,
+        sync::atomic::{AtomicU32, Ordering::Relaxed},
+    };
+
+    pub(super) struct JobServer(AtomicU32);
+
+    impl JobServer {
+        pub(super) fn new() -> Self {
+            // Use `NUM_JOBS` if set (it's configured by Cargo) and otherwise
+            // just fall back to a semi-reasonable number.
+            //
+            // Note that we could use `num_cpus` here but it's an extra
+            // dependency that will almost never be used, so
+            // it's generally not too worth it.
+            let mut parallelism = 4;
+            if let Ok(amt) = var("NUM_JOBS") {
+                if let Ok(amt) = amt.parse() {
+                    parallelism = amt;
+                }
+            }
+
+            Self(AtomicU32::new(parallelism))
+        }
+
+        pub(super) fn try_acquire(&self) -> Option<JobToken> {
+            let res = self.0.fetch_update(Relaxed, Relaxed, |tokens| {
+                if tokens > 0 {
+                    Some(tokens - 1)
+                } else {
+                    None
+                }
+            });
+
+            if res.is_ok() {
+                Some(JobToken(self))
+            } else {
+                None
+            }
+        }
+    }
+
+    pub(super) struct JobToken(&'static JobServer);
+
+    impl Drop for JobToken {
+        fn drop(&mut self) {
+            self.0 .0.fetch_add(1, Relaxed);
+        }
+    }
 }
---
+++ src/job_token/windows.rs
@@ -0,0 +1,62 @@
+use std::{
+    ffi::{CString, OsString},
+    io, ptr,
+};
+
+use crate::windows_sys::{
+    OpenSemaphoreA, ReleaseSemaphore, WaitForSingleObject, FALSE, HANDLE, SEMAPHORE_MODIFY_STATE,
+    THREAD_SYNCHRONIZE, WAIT_OBJECT_0,
+};
+
+pub(super) struct JobServerClient {
+    sem: HANDLE,
+}
+
+unsafe impl Sync for JobServerClient {}
+unsafe impl Send for JobServerClient {}
+
+impl JobServerClient {
+    pub(super) unsafe fn open(var: OsString) -> Option<Self> {
+        let s = var
+            .to_str()?
+            .split_ascii_whitespace()
+            .filter_map(|arg| {
+                arg.strip_prefix("--jobserver-fds=")
+                    .or_else(|| arg.strip_prefix("--jobserver-auth="))
+            })
+            .find(|s| !s.is_empty())?;
+
+        let name = CString::new(s).ok()?;
+
+        let sem = OpenSemaphoreA(
+            THREAD_SYNCHRONIZE | SEMAPHORE_MODIFY_STATE,
+            FALSE,
+            name.as_bytes().as_ptr(),
+        );
+        if sem != ptr::null_mut() {
+            Some(Self { sem })
+        } else {
+            None
+        }
+    }
+
+    pub(super) fn try_acquire(&self) -> io::Result<Option<()>> {
+        let r = unsafe { WaitForSingleObject(self.sem, 0) };
+        if r == WAIT_OBJECT_0 {
+            Ok(Some(()))
+        } else {
+            Err(io::Error::last_os_error())
+        }
+    }
+
+    pub(super) fn release(&self) -> io::Result<()> {
+        // SAFETY: ReleaseSemaphore will write to prev_count is it is Some
+        // and release semaphore self.sem by 1.
+        let r = unsafe { ReleaseSemaphore(self.sem, 1, ptr::null_mut()) };
+        if r != 0 {
+            Ok(())
+        } else {
+            Err(io::Error::last_os_error())
+        }
+    }
+}
