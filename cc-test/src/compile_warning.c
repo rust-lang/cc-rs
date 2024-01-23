690269a41e10c7a2dc5fe3ceb9582374b52b9377
Simon Berger <simon.berger@inomotech.com>
2024-01-23 13:05:35 +0100

    msvc-compatible warnings

--- cc-test/src/compile_warning.c
+++ cc-test/src/compile_warning.c
@@ -1,3 +1,7 @@
-#warning "if you see this, cargo_warnings(false) didn't do its job"
+#if _MSC_VER
+#   pragma message("if you see this, cargo_warnings(false) didn't do its job")
+#else
+#   warning "if you see this, cargo_warnings(false) didn't do its job"
+#endif
 
 void compile_warning(void) {}
