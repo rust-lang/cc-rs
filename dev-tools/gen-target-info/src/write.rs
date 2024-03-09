22ef3fe1d82c2c0e63d621a34810b1b5b4ddf87e
Jiahao XU <Jiahao_XU@outlook.com>
2024-03-09 18:35:48 +1100

    Fix `gen-target-info`: Generate formatted rust code
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- dev-tools/gen-target-info/src/write.rs
+++ dev-tools/gen-target-info/src/write.rs
@@ -1,10 +1,10 @@
 use std::{fmt::Write as _, fs, io::Write as _};
 
 pub fn write_string_mapping_to_file(f: &mut fs::File, variable_name: &str, data: &[(&str, &str)]) {
-    let mut content = format!("pub const {variable_name}: &[(&str, &str)] = &[");
+    let mut content = format!("pub const {variable_name}: &[(&str, &str)] = &[\n");
 
     for (f1, f2) in data {
-        write!(&mut content, r#"("{f1}", "{f2}"),"#).unwrap();
+        write!(&mut content, r#"    ("{f1}", "{f2}"),\n"#).unwrap();
     }
 
     content.push_str("];\n");
