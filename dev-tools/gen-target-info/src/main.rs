2cc4f4a0f42fbced3abf13c57a07c4cb396cc98a
Jiahao XU <Jiahao_XU@outlook.com>
2024-03-11 00:10:59 +1100

    Refactor: Extract new fn `generate_riscv_arch_mapping`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- dev-tools/gen-target-info/src/main.rs
+++ dev-tools/gen-target-info/src/main.rs
@@ -1,16 +1,7 @@
-use gen_target_info::{get_target_specs_from_json, write_target_tuple_mapping};
+use gen_target_info::{get_target_specs_from_json, write_target_tuple_mapping, RustcTargetSpecs};
 use std::{fs::File, io::Write as _};
 
-fn main() {
-    let target_specs = get_target_specs_from_json();
-
-    // Open file to write to
-    let manifest_dir = env!("CARGO_MANIFEST_DIR");
-
-    let path = format!("{manifest_dir}/../../src/target_info.rs");
-    let mut f = File::create(path).expect("failed to create src/target_info.rs");
-
-    // Wrute riscv target mapping
+fn generate_riscv_arch_mapping(f: &mut File, target_specs: &RustcTargetSpecs) {
     let mut riscv_target_mapping = target_specs
         .0
         .iter()
@@ -22,7 +13,20 @@ fn main() {
         .collect::<Vec<_>>();
     riscv_target_mapping.sort_unstable_by_key(|(arch, _)| &**arch);
     riscv_target_mapping.dedup();
-    write_target_tuple_mapping(&mut f, "RISCV_ARCH_MAPPING", &riscv_target_mapping);
+    write_target_tuple_mapping(f, "RISCV_ARCH_MAPPING", &riscv_target_mapping);
+}
+
+fn main() {
+    let target_specs = get_target_specs_from_json();
+
+    // Open file to write to
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+
+    let path = format!("{manifest_dir}/../../src/target_info.rs");
+    let mut f = File::create(path).expect("failed to create src/target_info.rs");
+
+    // Start generating
+    generate_riscv_arch_mapping(&mut f, &target_specs);
 
     // Flush the data onto disk
     f.flush().unwrap();
