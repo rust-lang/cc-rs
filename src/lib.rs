41a87317593083e0e41ca33fe2209f99c3fef80b
C191239 <zsz2007314zsz@sina.com>
2024-03-12 22:13:32 +0800

    fix xcrun sdk version arg

--- src/lib.rs
+++ src/lib.rs
@@ -3616,7 +3616,7 @@ impl Build {
 
             let version = run_output(
                 self.cmd("xcrun")
-                    .arg("--show-sdk-platform-version")
+                    .arg("--show-sdk-version")
                     .arg("--sdk")
                     .arg(sdk),
                 "xcrun",
