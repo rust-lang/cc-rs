173e649f8e7a875f012e7131cb2705b6febb9e9f
Jiahao XU <Jiahao_XU@outlook.com>
2024-04-18 21:19:59 +1000

    Add `parallel::OnceLock` impl copied from `std::sync::OnceLock`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/parallel/mod.rs
+++ src/parallel/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod async_executor;
 pub(crate) mod job_token;
+pub(crate) mod once_lock;
 pub(crate) mod stderr;
---
+++ src/parallel/once_lock.rs
@@ -0,0 +1,117 @@
+//! Copied from `std::sync::OnceLock`
+//! TODO: Remove this once `OnceLock::get_or_try_init`
+//! is initialised and MSRV is high enoguh to use it.
+
+use std::{
+    cell::UnsafeCell, convert::Infallible, marker::PhantomData, mem::MaybeUninit, sync::Once,
+};
+
+pub struct OnceLock<T> {
+    once: Once,
+    // Whether or not the value is initialized is tracked by `once.is_completed()`.
+    value: UnsafeCell<MaybeUninit<T>>,
+    /// `PhantomData` to make sure dropck understands we're dropping T in our Drop impl.
+    ///
+    /// ```compile_fail,E0597
+
+    /// use std::sync::OnceLock;
+    ///
+    /// struct A<'a>(&'a str);
+    ///
+    /// impl<'a> Drop for A<'a> {
+    ///     fn drop(&mut self) {}
+    /// }
+    ///
+    /// let cell = OnceLock::new();
+    /// {
+    ///     let s = String::new();
+    ///     let _ = cell.set(A(&s));
+    /// }
+    /// ```
+    _marker: PhantomData<T>,
+}
+
+impl<T> OnceLock<T> {
+    pub const fn new() -> OnceLock<T> {
+        OnceLock {
+            once: Once::new(),
+            value: UnsafeCell::new(MaybeUninit::uninit()),
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn get(&self) -> Option<&T> {
+        if self.is_initialized() {
+            // Safe b/c checked is_initialized
+            Some(unsafe { self.get_unchecked() })
+        } else {
+            None
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn get_or_init<F>(&self, f: F) -> &T
+    where
+        F: FnOnce() -> T,
+    {
+        match self.get_or_try_init(|| Ok::<T, Infallible>(f())) {
+            Ok(val) => val,
+            Err(err) => match err {},
+        }
+    }
+
+    pub fn get_or_try_init<F, E>(&self, f: F) -> Result<&T, E>
+    where
+        F: FnOnce() -> Result<T, E>,
+    {
+        // Fast path check
+        // NOTE: We need to perform an acquire on the state in this method
+        // in order to correctly synchronize `LazyLock::force`. This is
+        // currently done by calling `self.get()`, which in turn calls
+        // `self.is_initialized()`, which in turn performs the acquire.
+        if let Some(value) = self.get() {
+            return Ok(value);
+        }
+        self.initialize(f)?;
+
+        debug_assert!(self.is_initialized());
+
+        // SAFETY: The inner value has been initialized
+        Ok(unsafe { self.get_unchecked() })
+    }
+
+    #[inline]
+    fn is_initialized(&self) -> bool {
+        self.once.is_completed()
+    }
+
+    #[cold]
+    fn initialize<F, E>(&self, f: F) -> Result<(), E>
+    where
+        F: FnOnce() -> Result<T, E>,
+    {
+        let mut res: Result<(), E> = Ok(());
+        let slot = &self.value;
+
+        // Ignore poisoning from other threads
+        // If another thread panics, then we'll be able to run our closure
+        self.once.call_once_force(|_| match f() {
+            Ok(value) => {
+                unsafe { (&mut *slot.get()).write(value) };
+            }
+            Err(e) => {
+                res = Err(e);
+            }
+        });
+        res
+    }
+
+    /// # Safety
+    ///
+    /// The value must be initialized
+    #[inline]
+    unsafe fn get_unchecked(&self) -> &T {
+        debug_assert!(self.is_initialized());
+        (&*self.value.get()).assume_init_ref()
+    }
+}
