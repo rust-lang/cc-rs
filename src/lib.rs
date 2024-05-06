16aa23850d05ac91197433c8ffec479c560fd049
Jiahao XU <Jiahao_XU@outlook.com>
2024-05-06 12:36:19 +1000

    fix compilation

--- src/lib.rs
+++ src/lib.rs
@@ -3656,7 +3656,7 @@ impl Build {
         // Code copied from rustc's compiler/rustc_codegen_ssa/src/back/link.rs.
         if let Some(sdkroot) = env::var_os("SDKROOT") {
             let p = PathBuf::from(sdkroot);
-            let sdkroot = sdkroot.to_string_lossy();
+            let sdkroot = p.to_string_lossy();
             match sdk {
                 // Ignore `SDKROOT` if it's clearly set for the wrong platform.
                 "appletvos"
