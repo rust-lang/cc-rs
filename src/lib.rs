9c358039e969cd0d5b60a43ccc6d3a8f5eeaa30d
Jiahao XU <Jiahao_XU@outlook.com>
2024-06-21 19:07:16 +1000

    Update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1920,7 +1920,7 @@ impl Build {
                         cmd.push_cc_arg("-fno-plt".into());
                     }
                 }
-                if target == "wasm32-wasi" {
+                if target == "wasm32-wasip1" {
                     // WASI does not support exceptions yet.
                     // https://github.com/WebAssembly/exception-handling
                     cmd.push_cc_arg("-fno-exceptions".into());
