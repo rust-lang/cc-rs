a412806aaf822e849e4869b513112a4c009e918f
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2024-07-14 11:06:56 +1000

    Revert "Use raw-dylib for windows-sys (#1137)" (#1157)

--- src/windows/windows_targets.rs
+++ src/windows/windows_targets.rs
@@ -9,8 +9,7 @@ macro_rules! link_macro {
         // have in this repo. So instead we always link kernel32.lib and add the rest of the import
         // libraries below by using an empty extern block. This works because extern blocks are not
         // connected to the library given in the #[link] attribute.
-        #[cfg_attr(not(target_arch = "x86"), link(name = $library, kind = "raw-dylib", modifiers = "+verbatim"))]
-        #[cfg_attr(target_arch = "x86", link(name = $library, kind = "raw-dylib", modifiers = "+verbatim", import_name_type = "undecorated"))]
+        #[link(name = "kernel32")]
         extern $abi {
             $(#[link_name=$link_name])?
             pub fn $($function)*;
