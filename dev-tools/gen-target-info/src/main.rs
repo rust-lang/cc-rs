1c100131bf5632ac58eadeb5a94619a02aa8dbfa
Mateusz Mikuła <mati865@users.noreply.github.com>
2024-08-11 03:00:09 +0200

    Remap Windows targets triples to their LLVM counterparts (#1176)

--- dev-tools/gen-target-info/src/main.rs
+++ dev-tools/gen-target-info/src/main.rs
@@ -21,6 +21,22 @@ fn generate_riscv_arch_mapping(f: &mut File, target_specs: &RustcTargetSpecs) {
     write_target_tuple_mapping(f, "RISCV_ARCH_MAPPING", &riscv_target_mapping);
 }
 
+fn generate_windows_triple_mapping(f: &mut File, target_specs: &RustcTargetSpecs) {
+    let mut windows_target_mapping = target_specs
+        .0
+        .iter()
+        .filter_map(|(target, target_spec)| {
+            let rust_target_parts = target.splitn(4, '-').collect::<Vec<_>>();
+            let os = *rust_target_parts.get(2)?;
+            (os.contains("windows") && target != &*target_spec.llvm_target)
+                .then_some((&**target, &*target_spec.llvm_target))
+        })
+        .collect::<Vec<_>>();
+    windows_target_mapping.sort_unstable_by_key(|(triple, _)| &**triple);
+    windows_target_mapping.dedup();
+    write_target_tuple_mapping(f, "WINDOWS_TRIPLE_MAPPING", &windows_target_mapping);
+}
+
 fn main() {
     let target_specs = get_target_specs_from_json();
 
@@ -34,6 +50,7 @@ fn main() {
 
     // Start generating
     generate_riscv_arch_mapping(&mut f, &target_specs);
+    generate_windows_triple_mapping(&mut f, &target_specs);
 
     // Flush the data onto disk
     f.flush().unwrap();
