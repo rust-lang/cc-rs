b40a3e07ef5cb73b8e8f66ec168c45a036c4060f
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2025-08-02 23:07:27 +0200

    Add m68k-unknown-linux-gnu cross-compile target

--- src/lib.rs
+++ src/lib.rs
@@ -3545,6 +3545,7 @@ impl Build {
                     "i686-unknown-linux-musl" => Some("musl"),
                     "i686-unknown-netbsd" => Some("i486--netbsdelf"),
                     "loongarch64-unknown-linux-gnu" => Some("loongarch64-linux-gnu"),
+                    "m68k-unknown-linux-gnu" => Some("m68k-linux-gnu"),
                     "mips-unknown-linux-gnu" => Some("mips-linux-gnu"),
                     "mips-unknown-linux-musl" => Some("mips-linux-musl"),
                     "mipsel-unknown-linux-gnu" => Some("mipsel-linux-gnu"),
