2ee829021cd87a7b2a4fd25e1aa1e21b469a099c
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-08-11 21:49:12 +1000

    Fix llvm.rs

--- src/target/llvm.rs
+++ src/target/llvm.rs
@@ -91,9 +91,10 @@ impl TargetInfo<'_> {
         let env = match self.env {
             "newlib" | "nto70" | "nto71" | "nto71_iosock" | "p1" | "p2" | "relibc" | "sgx"
             | "uclibc" => "",
+            "sim" => "simulator",
             env => env,
         };
-        let abi = match self.abi {
+        let mut abi = match self.abi {
             "sim" => "simulator",
             "llvm" | "softfloat" | "uwp" | "vec-extabi" => "",
             "ilp32" => "_ilp32",
