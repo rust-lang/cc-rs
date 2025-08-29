8570df5ef3354a03d0394327e52360b13caa0823
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-08-30 01:48:10 +1000

    fix building for aarch64-apple-visionos-sim on nightly
    fix duplicate simulator suffix

--- src/target/llvm.rs
+++ src/target/llvm.rs
@@ -95,7 +95,7 @@ impl TargetInfo<'_> {
             env => env,
         };
         let abi = match self.abi {
-            "sim" => "simulator",
+            "sim" if env != "simulator" => "simulator",
             "llvm" | "softfloat" | "uwp" | "vec-extabi" => "",
             "ilp32" => "_ilp32",
             "abi64" => "",
