62c6e793d0118b5bcc78019eca61b7b21a39fb42
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-07-30 21:53:29 +1000

    Use constant for MSRV in mod read of gen-target-info (#1810)

--- dev-tools/gen-target-info/src/read.rs
+++ dev-tools/gen-target-info/src/read.rs
@@ -2,9 +2,11 @@ use std::process;
 
 use crate::{RustcTargetSpecs, TargetSpec};
 
+const MSRV: &'static str = "+1.64";
+
 pub fn get_targets_msrv() -> Vec<u8> {
     let mut cmd = process::Command::new("rustc");
-    cmd.args(["+1.64", "--print", "target-list"]);
+    cmd.args([MSRV, "--print", "target-list"]);
     cmd.stdout(process::Stdio::piped());
     cmd.stderr(process::Stdio::inherit());
 
@@ -20,7 +22,7 @@ pub fn get_targets_msrv() -> Vec<u8> {
 pub fn get_target_spec_from_msrv(target: &str) -> TargetSpec {
     let mut cmd = process::Command::new("rustc");
     cmd.args([
-        "+1.64",
+        MSRV,
         "-Zunstable-options",
         "--print",
         "target-spec-json",
