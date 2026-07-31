ba4b4112d2a65357097338bc5c59750401e99ced
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-07-31 12:59:46 +0100

    clippy: fix ptr_as_ptr lint
    https://rust-lang.github.io/rust-clippy/master/#ptr_as_ptr

--- find-msvc-tools/src/com.rs
+++ find-msvc-tools/src/com.rs
@@ -47,7 +47,7 @@ where
     }
     /// For internal use only.
     fn as_unknown(&self) -> &IUnknown {
-        unsafe { &*(self.0 as *mut IUnknown) }
+        unsafe { &*self.0.cast() }
     }
     /// Performs `QueryInterface` fun.
     pub fn cast<U>(&self) -> Result<ComPtr<U>, i32>
@@ -59,7 +59,7 @@ where
         if err < 0 {
             return Err(err);
         }
-        Ok(unsafe { ComPtr::from_raw(obj as *mut U) })
+        Ok(unsafe { ComPtr::from_raw(obj.cast()) })
     }
 }
 impl<T> Deref for ComPtr<T>
