c20feddb4f25567c488478cf12e1d95cbb7eef87
usamoi <usamoi@outlook.com>
2026-08-08 00:46:29 +0800

    do not emit -mno-omit-leaf-frame-pointer if unsupported (#1845)

--- src/lib.rs
+++ src/lib.rs
@@ -2264,7 +2264,16 @@ impl Build {
 
         if self.get_force_frame_pointer() {
             let family = cmd.family;
-            family.add_force_frame_pointer(cmd);
+            if let ToolFamily::Gnu | ToolFamily::Clang { .. } = family {
+                cmd.push_cc_arg("-fno-omit-frame-pointer".into());
+                let flag = OsString::from("-mno-omit-leaf-frame-pointer");
+                if self
+                    .is_flag_supported_inner(&flag, cmd, target)
+                    .unwrap_or(false)
+                {
+                    cmd.push_cc_arg(flag);
+                }
+            }
         }
 
         if !cmd.is_like_msvc() {
