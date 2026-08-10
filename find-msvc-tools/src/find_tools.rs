8a666c6da287221a6535774024110a851372faa3
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-08-10 19:58:57 +0100

    clippy: fix manual_let_else lint
    https://rust-lang.github.io/rust-clippy/master/#manual_let_else

--- find-msvc-tools/src/find_tools.rs
+++ find-msvc-tools/src/find_tools.rs
@@ -561,9 +561,7 @@ mod impl_ {
         version: &'static str,
         env_getter: &dyn EnvGetter,
     ) -> Box<dyn Iterator<Item = PathBuf>> {
-        let instances = if let Some(instances) = vs15plus_instances(target, env_getter) {
-            instances
-        } else {
+        let Some(instances) = vs15plus_instances(target, env_getter) else {
             return Box::new(iter::empty());
         };
         Box::new(instances.into_iter().filter_map(move |instance| {
@@ -1255,10 +1253,7 @@ mod impl_ {
             let val = subkey
                 .to_str()
                 .and_then(|s| s.trim_start_matches('v').replace('.', "").parse().ok());
-            let val = match val {
-                Some(s) => s,
-                None => continue,
-            };
+            let Some(val) = val else { continue };
             if val > max_vers {
                 if let Ok(k) = key.open(&subkey) {
                     max_vers = val;
