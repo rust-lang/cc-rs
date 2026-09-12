692c1ca5dcfaae52e8bb9ec4e35b7ef9bdb772b5
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2026-09-12 12:09:44 +1000

    Fix use of windows_bindgen::bindgen()

--- dev-tools/gen-windows-sys-binding/src/lib.rs
+++ dev-tools/gen-windows-sys-binding/src/lib.rs
@@ -39,8 +39,7 @@ pub fn generate_bindings() {
         "--filter",
         "--etc",
         &filter,
-    ])
-    .unwrap();
+    ]);
 
     let bindings =
         fs::read_to_string(temp_file.path()).expect("failed to read temp windows_sys.rs");
